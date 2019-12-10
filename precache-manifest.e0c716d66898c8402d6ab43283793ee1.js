d621ac9046d8d2da4a2c8550ff0ca67cc10ada08
unknown <bitebstephane1996@gmail.com>
2019-12-10 09:27:58 +0000

    Updates

---
+++ precache-manifest.e0c716d66898c8402d6ab43283793ee1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b1e7207f27cc22564fe5f853b862f17",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "349bfeb82db47fe4cfb7",
+    "url": "/monster-rolodex/static/css/main.77d9ee70.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monster-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "349bfeb82db47fe4cfb7",
+    "url": "/monster-rolodex/static/js/main.7c91de23.chunk.js"
+  },
+  {
+    "revision": "23fb47eb6f86565db1b0",
+    "url": "/monster-rolodex/static/js/runtime-main.47b0ddc3.js"
+  }
+]);
